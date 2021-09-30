0e13e06c2f0351ba63cdf109c0c89d4ada5c4ded
InSeong-So <goflvhxj2547@gmail.com>
2021-09-30 19:18:11 +0900

    ✏️ 나만의 웹팩 만들기(sass, VanillaJS)

--- my_webpack/src/index.js
+++ my_webpack/src/index.js
@@ -1,7 +1,9 @@
+import '../assets/scss/index.scss';
+
 const openButton = document.querySelector('button');
 const modal = document.querySelector('.modal');
 const closeButton = modal.querySelector('button');
-const modalBackground = modal.querySelector('.modal__background');
+const modalBackground = modal.querySelector('.background');
 
 function displayModal() {
   modal.classList.toggle('hidden');
