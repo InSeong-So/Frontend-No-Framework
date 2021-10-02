8070808233ff71b1dda66ee5beb5b5a9b6976441
InSeong-So <goflvhxj2547@gmail.com>
2021-10-02 18:31:26 +0900

    💻 HTTP Client 구현

---
+++ _theory/http-request/src/client/AxiosClient.js
@@ -0,0 +1,69 @@
+const request = async params => {
+  const { method = 'GET', url, headers = {}, body } = params;
+
+  const config = {
+    url,
+    method,
+    headers,
+    data: body,
+  };
+
+  return axios(config);
+};
+
+const get = async (url, headers) => {
+  const response = await request({
+    url,
+    headers,
+    method: 'GET',
+  });
+
+  return response.data;
+};
+
+const post = async (url, body, headers) => {
+  const response = await request({
+    url,
+    headers,
+    method: 'POST',
+    body,
+  });
+  return response.data;
+};
+
+const put = async (url, body, headers) => {
+  const response = await request({
+    url,
+    headers,
+    method: 'PUT',
+    body,
+  });
+  return response.data;
+};
+
+const patch = async (url, body, headers) => {
+  const response = await request({
+    url,
+    headers,
+    method: 'PATCH',
+    body,
+  });
+  return response.data;
+};
+
+const deleteRequest = async (url, headers) => {
+  const response = await request({
+    url,
+    headers,
+    method: 'DELETE',
+  });
+  return response.data;
+};
+
+export const http = {
+  get,
+  post,
+  put,
+  patch,
+  delete: deleteRequest,
+};
