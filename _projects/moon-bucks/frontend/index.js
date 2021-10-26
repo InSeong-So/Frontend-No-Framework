5e17738f2ca0397a9d39e844b2de3b30591dcd93
InSeong-So <goflvhxj2547@gmail.com>
2021-10-26 23:38:56 +0900

    💻 프론트엔드 파일 업로드 추가 중

--- _projects/moon-bucks/frontend/index.js
+++ _projects/moon-bucks/frontend/index.js
@@ -1,5 +1,6 @@
 import setGlobalEventHandler from './src/events/index.js';
 import App from './src/app.js';
 
-setGlobalEventHandler();
 new App(document.querySelector('#app'));
+
+setGlobalEventHandler();
--- _projects/moon-bucks/frontend/src/components/MenuList.js
+++ _projects/moon-bucks/frontend/src/components/MenuList.js
@@ -29,7 +29,13 @@ export default class MenuList extends Component {
           </button>
           <button
             type="button"
-            class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+            class="bg-gray-50 text-gray-500 text-sm mr-1 menu-image-button open"
+          >
+            이미지
+          </button>
+          <button
+            type="button"
+            class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button open"
           >
             수정
           </button>
