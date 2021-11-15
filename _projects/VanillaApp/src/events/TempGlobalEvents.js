87e6104f787e9a0a55dea5222c83c948992f7b73
InSeong-So <goflvhxj2547@gmail.com>
2021-11-15 12:09:52 +0900

    🏷 최종: 초기 템플릿 완성

---
+++ _projects/VanillaApp/src/events/TempGlobalEvents.js
@@ -0,0 +1,25 @@
+export default (() => {
+  window.addEventListener('scroll', () => {
+    var windowTop = document.documentElement.scrollTop;
+    windowTop > 100
+      ? document.querySelector('nav').classList.add('navShadow')
+      : document.querySelector('nav').classList.remove('navShadow');
+    windowTop > 100
+      ? (document.querySelector('ul').style.top = '90px')
+      : (document.querySelector('ul').style.top = '120px');
+  });
+
+  document.querySelector('#logo').addEventListener('click', () => {
+    location.replace('http://127.0.0.1:5500/_projects/VanillaApp/index.html');
+  });
+
+  document.querySelector('#menu-toggle').addEventListener('click', () => {
+    document.querySelector('#menu-toggle').classList.toggle('closeMenu');
+    document.querySelector('ul').classList.toggle('showMenu');
+
+    document.querySelector('li').addEventListener('click', () => {
+      document.querySelector('ul').removeClass('showMenu');
+      document.querySelector('#menu-toggle').removeClass('closeMenu');
+    });
+  });
+})();
