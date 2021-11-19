2778804ab0f05eb3991039615346b39f18221834
InSeong-So <goflvhxj2547@gmail.com>
2021-11-19 17:36:32 +0900

    🧪 불변 객체 테스트

---
+++ _test/invarint/v2/main.js
@@ -0,0 +1,39 @@
+import produce from './index.js';
+
+const state = {
+  posts: [
+    {
+      id: 1,
+      title: '제목입니다.',
+      body: '내용입니다.',
+      comments: [
+        {
+          id: 1,
+          text: '와 정말 잘 읽었습니다.',
+        },
+      ],
+    },
+    {
+      id: 2,
+      title: '제목입니다.',
+      body: '내용입니다.',
+      comments: [
+        {
+          id: 2,
+          text: '또 다른 댓글 어쩌고 저쩌고',
+        },
+      ],
+    },
+  ],
+  selectedId: 1,
+};
+
+const nextState = produce(state, draft => {
+  const post = draft.posts.find(post => post.id === 1);
+  post.comments.push({
+    id: 3,
+    text: '와 정말 쉽다!',
+  });
+});
+
+console.log(nextState);
